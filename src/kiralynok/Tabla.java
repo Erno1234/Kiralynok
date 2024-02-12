b09a324b71dfe79e0dc3c734eefa78f34444df94
Erno1234 <palocierno12>
2024-02-12 13:38:57 +0100

    2. Feladat

--- src/kiralynok/Tabla.java
+++ src/kiralynok/Tabla.java
@@ -6,14 +6,27 @@ public class Tabla {
     private char uresCella;
 
     public Tabla(char uresCella) {
-        T = new char[8][8];
-        this.uresCella = uresCella;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                T[i][j] = uresCella;
-            }
-        }
     }
-    
-    
+
+    public void megjelenit() {
+    }
+
+    public void elhelyez(int n) {
+    }
+
+    public boolean uresSor(int sor) {
+        return false;
+    }
+
+    public boolean uresOszlop(int oszlop) {
+        return false;
+    }
+
+    public int uresSorokSzama() {
+        return 0;
+    }
+
+    public int uresOszlopokSzama() {
+        return 0;
+    }
 }
